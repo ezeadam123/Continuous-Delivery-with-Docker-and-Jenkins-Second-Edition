e16224dc283a77533f9e94a0b67e2cec9c43afb3
Adam Asimolowo <64811750+ezeadam123@users.noreply.github.com>
2023-03-16 06:48:03 -0400

    Update CalculatorTest.java

--- Chapter08/sample1/src/test/java/com/leszko/calculator/CalculatorTest.java
+++ Chapter08/sample1/src/test/java/com/leszko/calculator/CalculatorTest.java
@@ -7,9 +7,9 @@ public class CalculatorTest {
   * Tests if the results of a calculator 
   *are true/false(in this instance, is 2+3 = 5)
   */
-     private Calculator calculator = new Calculator();
-     @Test
-     public void testSum() {
-          assertEquals(5, calculator.sum(2, 3));
-     }
+  private Calculator calculator = new Calculator();
+  @Test
+  public void testSum() {
+   assertEquals(5, calculator.sum(2, 3));
+  }
 }

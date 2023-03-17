f5b8cc2f651efff49591c3693f4cb3a3a7d3942e
Adam Asimolowo <64811750+ezeadam123@users.noreply.github.com>
2023-03-16 23:52:38 -0400

    Update CalculatorTest.java

--- Chapter08/sample1/src/test/java/com/leszko/calculator/CalculatorTest.java
+++ Chapter08/sample1/src/test/java/com/leszko/calculator/CalculatorTest.java
@@ -2,12 +2,18 @@ package com.leszko.calculator;
 import org.junit.Test;
 import static org.junit.Assert.assertEquals;
 
-/** Represents a calculator test being ran to ensure
+/** 
+* Represents a calculator test being ran to ensure
 * the given sum of 2 and 3 is equal to 5
 */
 public class CalculatorTest {
+     /**
+     * Creating a calculator object to utilize our package
+     */
      private Calculator calculator = new Calculator();
-     /** Checks to see whether the sum of 2 and 3
+     
+     /** 
+     * Checks to see whether the sum of 2 and 3
      * is equal to 5. nothing is returned
      */
      @Test

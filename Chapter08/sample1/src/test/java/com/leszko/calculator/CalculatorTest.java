3c8ad31ef8105a8edb89b1ae31f33a381e577969
Adam Asimolowo <64811750+ezeadam123@users.noreply.github.com>
2023-03-16 22:14:11 -0400

    Update CalculatorTest.java

--- Chapter08/sample1/src/test/java/com/leszko/calculator/CalculatorTest.java
+++ Chapter08/sample1/src/test/java/com/leszko/calculator/CalculatorTest.java
@@ -1,8 +1,3 @@
-/**
-*Checking for Javadoc comments
-*This code will test to ensure the sum of (2,3) is 
-*equal to 5
-*/
 package com.leszko.calculator;
 import org.junit.Test;
 import static org.junit.Assert.assertEquals;
@@ -10,7 +5,12 @@ import static org.junit.Assert.assertEquals;
 public class CalculatorTest {
      
      private Calculator calculator = new Calculator();
- 
+     
+     /**
+     *Checking for Javadoc comments
+     *This code will test to ensure the sum of (2,3) is 
+     *equal to 5
+     */
      @Test
      public void testSum() {
           assertEquals(5, calculator.sum(2, 3));

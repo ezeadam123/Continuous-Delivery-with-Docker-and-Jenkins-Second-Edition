73362675bfce30435462850ed8d7304fee6297fd
Adam Asimolowo <64811750+ezeadam123@users.noreply.github.com>
2023-03-17 00:48:00 -0400

    Update Calculator.java

--- Chapter08/sample1/src/main/java/com/leszko/calculator/Calculator.java
+++ Chapter08/sample1/src/main/java/com/leszko/calculator/Calculator.java
@@ -3,6 +3,10 @@ package com.leszko.calculator;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 
+/**
+* Creating a calculator class that will take in two parameters
+* and add them up together.
+*/
 @Service
 public class Calculator {
         final static int umlNUMBER1 = 3;
